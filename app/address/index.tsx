832a60086b87ccc5158b3cec485c41735fe37c96
abhishandy <abhishek.sandy@gmail.com>
2023-09-06 22:43:05 -0400

    feat: setup bitcoin libs

--- app/address/index.tsx
+++ app/address/index.tsx
@@ -1,12 +1,21 @@
 import { useEffect, useState } from "react";
 import { View, StyleSheet, Text } from "react-native";
 import { getZpubsFromDB } from "../../lib/db";
+import * as bitcoin from "bitcoinjs-lib";
+import * as ecc from "@bitcoin-js/tiny-secp256k1-asmjs";
+import BIP32 from "bip32";
+const bip32 = BIP32(ecc);
 
 export default function Address() {
   const [address, setAddress] = useState<string>();
   useEffect(() => {
     const calculateFirstAddress = async () => {
-      const zpubs = await getZpubsFromDB();
+      const zpubs = (await getZpubsFromDB()) as { zpub: string }[];
+
+      if (!zpubs.length) {
+        return;
+      }
+      const network = bitcoin.networks.bitcoin;
     };
 
     calculateFirstAddress();
